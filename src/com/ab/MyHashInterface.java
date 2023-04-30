ac74b7a3b930b6e0550ad8ac8498d62f2f24058b
Zooom_15 <amangikbaktibaiuli@gmail.com>
2023-04-30 18:33:41 +0600

    Without white spaces

--- src/com/ab/MyHashInterface.java
+++ src/com/ab/MyHashInterface.java
@@ -1,16 +1,9 @@
 package com.ab;
 
 public interface MyHashInterface<K, V> {
-
     void put(K key, V value);
-
     V get(K key);
-
     V remove(K key);
-
     boolean contains(V value);
-
     K getKey(V value);
-
-
 }
