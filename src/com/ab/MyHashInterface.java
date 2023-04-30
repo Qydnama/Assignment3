6831e764c8e9f01420e9de3179a78d5ca0cf6e80
Zooom_15 <amangikbaktibaiuli@gmail.com>
2023-04-30 18:31:44 +0600

    MyHashInterface

---
+++ src/com/ab/MyHashInterface.java
@@ -0,0 +1,16 @@
+package com.ab;
+
+public interface MyHashInterface<K, V> {
+
+    void put(K key, V value);
+
+    V get(K key);
+
+    V remove(K key);
+
+    boolean contains(V value);
+
+    K getKey(V value);
+
+
+}
